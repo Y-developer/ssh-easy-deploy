6e13ebf76e4dfd62a59c30e553cfca6aa8670f9f
Yasitha Nadeeshan(wsl) <yasithanadeeshan@gmail.com>
2022-10-22 10:54:17 +0530

    fix: import packages

--- src/writeConfig.js
+++ src/writeConfig.js
@@ -1,7 +1,10 @@
+const fs = require("fs");
+const { configFileName } = require("./constant");
+
 function writeConfig(dataObj) {
     const content = JSON.stringify(dataObj, null, 1);
-    console.log(`>>>>>> Create new ${fileName} file`);
-    fs.writeFile(fileName, content, err => {
+    console.log(`>>>>>> Create new ${configFileName} file`);
+    fs.writeFile(configFileName, content, err => {
         if (err) {
             console.error(err);
             return;
